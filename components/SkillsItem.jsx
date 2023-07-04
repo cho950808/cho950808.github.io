08d97884a93b1ad870a0a06ae04aa7e9d68d8951
cho950808 <cho950808@naver.com>
2023-07-05 01:20:45 +0900

    [Fix] Header Sticky 수정

--- components/SkillsItem.jsx
+++ components/SkillsItem.jsx
@@ -1,19 +1,19 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 const SkillsItem = ({ urlImg, framework }) => {
-  return (
-    <div className="p-7 shadow-xl rounded-xl hover:scale-105 ease-in duration-100">
-      <div className="grid grid-cols-1 gap-3 justify-center items-center">
-        <div className="m-auto overflow-hidden h-[50px] w-[50px]">
-          <Image src={urlImg} width="100%" height="100%" alt="/" />
+    return (
+        <div className="p-7 shadow-xl rounded-xl hover:scale-105 ease-in duration-100">
+            <div className="grid grid-cols-1 gap-3 justify-center items-center">
+                <div className="m-auto overflow-hidden h-[50px] w-[50px]">
+                    <Image src={urlImg} width="100%" height="100%" alt="/" />
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <h3 className="uppercase">{framework}</h3>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <h3 className="uppercase">{framework}</h3>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SkillsItem;
