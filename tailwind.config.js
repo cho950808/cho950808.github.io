e8d1bd96fc92edd98ddb1b1b076430300d2dafa9
cho950808 <cho950808@naver.com>
2023-07-08 00:20:29 +0900

    [Feat] 프로젝트 문서 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    screens: {
+        xs: '576',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+    },
+    plugins: [],
 };
