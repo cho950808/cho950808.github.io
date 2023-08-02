662902e409da6057b61fa2949ca478069cbe75e5
cho950808 <cho950808@naver.com>
2023-08-02 15:27:55 +0900

    Updates

--- _next/static/8LiG5P7qE4e9ymKhQkvuL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-b512b8a3c52f5faa.js","static/chunks/pages/index-faa74ab8060c658f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
